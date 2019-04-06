2b10a3ac5b01e3387e95292769257d793ebc3cd7
Arthur Correnson <arthur.correnson@gmail.com>
2019-04-06 20:55:26 +0200

    Add recognition of deprecated SYS call

--- src/readRom.c
+++ src/readRom.c
@@ -41,119 +41,127 @@ void printMemory(unsigned short * memory, int size)
   }
 }
 
+void printOpCode(char * instruction, unsigned short const opCode)
+{
+  printf("OP_CODE : \033[35m%04X\033[0m  |  ASEMBLY : \033[35m%s\033[0m\n", opCode, instruction);
+}
+
 void desasembler(unsigned short const opCode)
 {
-  if(opCode == 0x00E0) {
-    printf("opCode : %04X  | asembler : CLS\n", opCode);
+  if(opCode >> 12 == 0) {
+    printOpCode("DEPRECATED SYS CALL", opCode);
+  }
+  else if(opCode == 0x00E0) {
+    printOpCode("CLS", opCode);
   }
   else if(opCode == 0x00EE) {
-    printf("opCode : %04X  | asembler : RET\n", opCode);
+    printOpCode("RET", opCode);
   }
   else if(opCode >> 12 == 1) {
-    printf("opCode : %04X  | asembler : JP\n", opCode);
+    printOpCode("JP", opCode);
   }
   else if(opCode >> 12 == 2) {
-    printf("opCode : %04X  | asembler : CALL\n", opCode);
+    printOpCode("CALL", opCode);
   }
   else if(opCode >> 12 == 3) {
-    printf("opCode : %04X  | asembler : SKP\n", opCode);
+    printOpCode("SKP", opCode);
   }
   else if(opCode >> 12 == 4) {
-    printf("opCode : %04X  | asembler : SKP\n", opCode);
+    printOpCode("SKP", opCode);
   }
   else if(opCode >> 12 == 5) {
-    printf("opCode : %04X  | asembler : SKP\n", opCode);
+    printOpCode("SKP", opCode);
   }
   else if(opCode >> 12 == 6) {
-    printf("opCode : %04X  | asembler : SET\n", opCode);
+    printOpCode("SET", opCode);
   }
   else if(opCode >> 12 == 7) {
-    printf("opCode : %04X  | asembler : ADD\n", opCode);
+    printOpCode("ADD", opCode);
   }
   else if(opCode >> 12 == 8)
   {
     if ( (opCode & 0x0000) == 0) {
       // LD Vx, Vy
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     }
     else if ( (opCode & 0x0001) == 1) {
-      printf("opCode : %04X  | asembler : OR\n", opCode);
+      printOpCode("OR", opCode);
     }
     else if ( (opCode & 0x0002) == 2) {
-      printf("opCode : %04X  | asembler : AND\n", opCode);
+      printOpCode("AND", opCode);
     }
     else if ( (opCode & 0x0003) == 3) {
-      printf("opCode : %04X  | asembler : XOR\n", opCode);
+      printOpCode("XOR", opCode);
     }
     else if ( (opCode & 0x0004) == 4) {
-      printf("opCode : %04X  | asembler : ADD\n", opCode);
+      printOpCode("ADD", opCode);
     }
     else if ( (opCode & 0x0005) == 5) {
-      printf("opCode : %04X  | asembler : SUB\n", opCode);
+      printOpCode("SUB", opCode);
     }
     else if ( (opCode & 0x0006) == 6) {
-      printf("opCode : %04X  | asembler : SHR\n", opCode);
+      printOpCode("SHR", opCode);
     }
     else if ( (opCode & 0x0007) == 7) {
-      printf("opCode : %04X  | asembler : SUBN\n", opCode);
+      printOpCode("SUBN", opCode);
     }
     else if ( (opCode & 0x0008) == 8) {
-      printf("opCode : %04X  | asembler : SHL\n", opCode);
+      printOpCode("SHL", opCode);
     }
   }
   else if (opCode >> 12 == 9) {
-    printf("opCode : %04X  | asembler : SKP\n", opCode);
+    printOpCode("SKP", opCode);
   }
   else if (opCode >> 12 == 0xA) {
-    printf("opCode : %04X  | asembler : LD\n", opCode);
+    printOpCode("LD", opCode);
   }
   else if (opCode >> 12 == 0xB) {
-    printf("opCode : %04X  | asembler : JP\n", opCode);
+    printOpCode("JP", opCode);
   }
   else if (opCode >> 12 == 0xC) {
-    printf("opCode : %04X  | asembler : RND\n", opCode);
+    printOpCode("RND", opCode);
   }
   else if (opCode >> 12 == 0xD) {
-    printf("opCode : %04X  | asembler : DRW\n", opCode);
+    printOpCode("DRW", opCode);
   }
   else if (opCode >> 12 == 0xE)
   {
     if ( (opCode & 0x009E) ==  0x009E ) {
-      printf("opCode : %04X  | asembler : SKP\n", opCode);
+      printOpCode("SKP", opCode);
     }
     else if ( (opCode & 0x00A1) == 0x00A1 ) {
-      printf("opCode : %04X  | asembler : SKNP\n", opCode);
+      printOpCode("SKNP", opCode);
     }
   }
   else if (opCode >> 12 == 0xF ) {
     if ( (opCode & 0x0007) == 0x0007 ) {
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x000A) == 0x000A ) {
       // LD Vx, K
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x0015) == 0x0015 ) {
       // LD DT, Vx
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x0018) == 0x0018 ) {
       // LD ST, Vx
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x001E) == 0x001E ) {
       // ADD I, Vx
-      printf("opCode : %04X  | asembler : ADD\n", opCode);
+      printOpCode("ADD", opCode);
     } else if ( (opCode & 0x0029) == 0x0029 ) {
       // LD F, Vx
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x0033) == 0x0033 ) {
       // LD B, Vx
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x0055) == 0x0055 ) {
       // LD [I], Vx
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     } else if ( (opCode & 0x0065) == 0x0065 ) {
       // LD Vx, [I]
-      printf("opCode : %04X  | asembler : LD\n", opCode);
+      printOpCode("LD", opCode);
     }
   } else {
-    printf("opCode : %04X  | asembler : NOT FOUND\n", opCode);
+    printOpCode("NOT FOUND", opCode);
   }
 }
